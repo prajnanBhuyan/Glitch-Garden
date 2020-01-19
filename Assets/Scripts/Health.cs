ca9fcc2d1b714dd6c66bee5c0d32d29906927f5e
Prajnan Jyoti Bhuyan <prajnanbhuyan@gmail.com>
2020-01-19 18:42:16 +0530

    added damage mechanism

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    int health = 100;
+
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,8 +7,21 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     float speed = 1f;
 
+    [SerializeField]
+    int damage = 50;
+
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+
+        if (health != null)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
