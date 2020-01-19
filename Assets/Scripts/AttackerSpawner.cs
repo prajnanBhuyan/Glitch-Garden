abaf276fd34d7adc3a1de77ab7537c53c800c912
Prajnan Jyoti Bhuyan <prajnanbhuyan@gmail.com>
2020-01-19 10:33:53 +0530

    added spawners

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    float minSpawnDelay = 1f;
+    [SerializeField]
+    float maxSpawnDelay = 5f;
+    [SerializeField]
+    Attacker attackerPrefab;
+
+    bool spawn = true;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
