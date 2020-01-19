fd139fc0965bcb7f1b1b64beabb7407538f855be
Prajnan Jyoti Bhuyan <prajnanbhuyan@gmail.com>
2020-01-19 18:01:46 +0530

    added projectiles

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [SerializeField] [Range(0f, 5f)]
-    float walkSpeed = 1f;
+    [Range(0f, 5f)]
+    float movementSpeed = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed  * Time.deltaTime);
+        transform.Translate(Vector2.left * movementSpeed  * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        movementSpeed = speed;
     }
 }
