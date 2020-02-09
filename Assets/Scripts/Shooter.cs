f26dbbc0325024806173d80d2b272ba999fc3d8a
Prajnan Jyoti Bhuyan <prajnanbhuyan@gmail.com>
2020-02-09 23:36:03 +0530

    tidying hierarchy

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -10,11 +10,24 @@ public class Shooter : MonoBehaviour
 
     AttackerSpawner myLaneSpawner;
     Animator animator;
+    GameObject projectileParent;
+
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
 
     private void Start()
     {
         SetLaneSpawner();
         animator = GetComponent<Animator>();
+        CreateProjectileParent();
+    }
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
     }
 
     private void Update()
@@ -48,6 +61,7 @@ public class Shooter : MonoBehaviour
 
     public void Fire()
     {
-        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+        GameObject newProjectile = Instantiate(projectile, gun.transform.position, gun.transform.rotation) as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
     }
 }
